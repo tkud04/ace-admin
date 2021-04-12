61a7da1c87cf98ec11597f70dd416c386c45fa40
Tobi K <32499735+tkud04@users.noreply.github.com>
2021-04-12 20:50:24 +0100

    Update confirm-payment.blade.php

--- resources/views/emails/confirm-payment.blade.php
+++ resources/views/emails/confirm-payment.blade.php
@@ -4,13 +4,13 @@ $totals = $order['totals'];
   $items = $order['items'];
   $cr = $order['courier'];
  $itemCount = $totals['items'];
-$h2 = $order['type'] == "pod" ? "Your order has been delivered and full payment received" : "Payment confirmed!";
+$h2 = $order['type'] == "pod" ? "Part payment received" : "Payment confirmed!";
 ?>
 <center><img src="http://www.aceluxurystore.com/images/logo.png" width="150" height="150"/></center>
 <h3 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">{{$h2}}</h3>
 Hello {{$name}},<br>
 @if($order['type'] == "pod")
-your payment for order <b>{{$order['reference']}}</b> is now complete. 
+The initial payment for your order <b>{{$order['reference']}}</b> has been received and your order is being processed.
 @else
  your payment for order <b>{{$order['reference']}}</b> has been cleared and your order is being processed.
 @endif
