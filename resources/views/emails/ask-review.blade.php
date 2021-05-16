b2fc0142e4befd0f3832d828518ceb69f8fdd326
Tkud <kudayisitobi@gmail.com>
2021-05-16 15:17:29 +0100

    adding order reviews

--- resources/views/emails/ask-review.blade.php
+++ resources/views/emails/ask-review.blade.php
@@ -3,7 +3,7 @@
 ?>
 <center><img src="http://www.aceluxurystore.com/images/logo.png" width="150" height="150"/></center>
 <h3 style="background: #ff9bbc; color: #fff; padding: 10px 15px;">{{$subject}}</h3>
-Hello {{$name}},<br> We want to say a big thank you for purchasing on our store! What did you think of your order? If you don't mind, we would like you to please leave a review here:<br><br>
+Hello {{$name}},<br> We want to say a big thank you for purchasing from our store! What did you think of your order? If you don't mind, we would like you to please leave a review here:<br><br>
 
 <a href="{{$uu}}" target="_blank" style="background: #ff9bbc; color: #fff; padding: 10px 15px; margin-right: 10px;">Leave a review</a><br><br>
 
