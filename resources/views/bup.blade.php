65d6d776e85ba3a02529162ad2958cb5c381b9eb
Tkud <kudayisitobi@gmail.com>
2021-11-16 13:51:08 +0100

    rounding up

--- resources/views/bup.blade.php
+++ resources/views/bup.blade.php
@@ -29,7 +29,7 @@
  $('.bup-hide').hide();
  
   //get fb permission
-   console.log("calling fb login.. ");
+   console.log("calling fb login.. ",FB);
 		FB.login(function(response) {
              // handle the response
 			 console.log("response: ", response);
